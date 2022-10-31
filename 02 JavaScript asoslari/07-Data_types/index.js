c1f6f7c11e5354e758d6e446abc0780e0a74a754
islomnumanov <islomnumanov.uz@gmail.com>
2022-10-31 21:22:32 +0300

    feat: Add data types

---
+++ 02 JavaScript asoslari/07-Data_types/index.js
@@ -0,0 +1,41 @@
+// data types 8
+// primitive 7 and non-primitive 1
+
+
+// number
+const number = 10;
+const float = 10.4;
+// NaN and Infinity also type is number
+
+// string
+const str = "JavaScript";
+
+// undefined
+let userName;
+
+// null
+let shoppingCart = null;
+
+// boolean
+const isMarried = false;
+
+// symbol
+let sym = new Symbol("label");
+
+// bigInt
+let bigIntNumber = 10n;
+
+//=================
+//=================
+
+// Non primite
+// object
+const user = {
+  name: "Samar",
+  status: "active",
+  rating: -1
+}
+
+
+// array
+const arr = [1, 5, true, "str"]
